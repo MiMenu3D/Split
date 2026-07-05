e52a56206b9572eb92553f853a37193f39855aee
MiMenu3D <mimenu3d@gmail.com>
2026-07-05 13:34:26 +0200

    Update xr-config.js

--- xr-config.js
+++ xr-config.js
@@ -1,4 +1,4 @@
-// 8th Wall image target configuration
+// 8th Wall image target configuration - Version Half Split 1.00
 // Only configure the tracker, do not auto-start the 8th Wall scene.
 window.xrConfigData = {
   imageTargetData: [
